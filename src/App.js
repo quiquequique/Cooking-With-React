3bee27123343ee935ec3e67e39c9f25d9f8219b7
quiquequique <eabramzon@gmail.com>
2022-02-25 14:44:45 -0300

    green function corrected

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
       <button onClick={() =>
         setTheme((prevTheme) => {
-        return prevTheme === 'red' ? 'blue' : 'green';
+        return prevTheme === 'green' ? 'blue' : 'green';
       })}>
         Toggle Theme
       </button>
